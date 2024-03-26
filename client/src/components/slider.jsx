e9bce0a44a317d92c88d3b8f077318b5bf059519
rebeccawaweru <wawerur95@gmail.com>
2024-03-26 21:51:00 +0300

    slider linear gradient & indicators

--- client/src/components/slider.jsx
+++ client/src/components/slider.jsx
@@ -47,29 +47,64 @@ const Slider = ({ images, autoSwipeInterval = 5000 }) => {
 
   return (
     <Box {...swipeHandlers} sx={{ position: 'relative', overflow: 'hidden' }}>
-      <div style={{ display: 'flex', overflowX: 'auto' }}>
+      <div style={{ position: 'relative', width: '100%', height: '100vh', overflow: 'hidden' }}>
         {images.map((image, index) => (
-          <img
+          <div key={index} style={{ display: index === currentIndex ? 'block' : 'none' }}>
+            <div
+              style={{
+                position: 'absolute',
+                top: 0,
+                left: 0,
+                width: '100%',
+                height: '100%',
+                backgroundImage: `linear-gradient(to right, rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.3)), url(${image})`,
+                backgroundSize: 'cover',
+                backgroundRepeat: 'no-repeat',
+                backgroundPosition: 'center',
+              }}
+            />
+            {/* Original image */}
+            <img
+              src={image}
+              alt={`Image ${index + 1}`}
+              style={{
+                objectFit: 'cover',
+                flexShrink: 0,
+                width: '100%',
+                maxWidth: '100%',
+                height: '100vh',
+              }}
+            />
+          </div>
+        ))}
+      </div>
+      {/* Dot indicator */}
+      <div style={{ textAlign: 'center', position: 'absolute', right: 0, left: 0, bottom: 0, transform: 'translateY(-50%)' }}>
+        {images.map((_, index) => (
+          <span
             key={index}
-            src={image}
-            alt={`Image ${index + 1}`}
             style={{
-              objectFit:"cover",
-              flexShrink: 0,
-              width: '100%',
-              maxWidth: '100%',
-              height: '100vh',
-              display: index === currentIndex ? 'block' : 'none' // Show only the current image
+              display: 'inline-block',
+              width: '15px',
+              height: '15px',
+              borderRadius: '50%',
+              border: index === currentIndex ? '2px solid white' : 'none',
+              backgroundColor: index === currentIndex ? 'whitesmoke' : 'gray', // Active dot color and inactive dot color
+              margin: '0 4px',
+              cursor: 'pointer',
             }}
-          />
+            onClick={() => setCurrentIndex(index)} // Clicking on the dot to navigate to the corresponding image
+          ></span>
         ))}
       </div>
       {/* Next button */}
-      <IconButton onClick={nextImage} sx={{ position: 'absolute', right: 0, top: '50%', transform: 'translateY(-50%)'}}><ArrowForwardIos fontSize='large'/></IconButton>
+      <IconButton onClick={nextImage} sx={{ position: 'absolute', right: 0, top: '50%', transform: 'translateY(-50%)' }}><ArrowForwardIos fontSize='large' /></IconButton>
       {/* Previous button */}
-      <IconButton size='extralarge' onClick={prevImage} sx={{ position: 'absolute', left: 0, top: '50%', transform: 'translateY(-50%)' }}><ArrowBackIosNew fontSize='large'/></IconButton>
+      <IconButton size='extralarge' onClick={prevImage} sx={{ position: 'absolute', left: 0, top: '50%', transform: 'translateY(-50%)' }}><ArrowBackIosNew fontSize='large' /></IconButton>
     </Box>
   );
+  
+  
 };
 
 export default Slider;
