70c850b210cacec8e235f4fb572157cde2d2732e
rebeccawaweru <wawerur95@gmail.com>
2024-04-08 12:06:53 +0300

    get request headers update, packages responsive design

--- client/src/pages/Home/LandingPage.jsx
+++ client/src/pages/Home/LandingPage.jsx
@@ -1,12 +1,12 @@
 import Wrapper from "../../layouts/Wrapper"
 import { useEffect, useState } from "react"
 import client from '../../api/client'
-import { Container, Box, Typography, Stack, Button } from "@mui/material"
+import { Container, Box, Typography, Stack } from "@mui/material"
 import { Slider, Quality, Package } from "../../components"
-import { content, whatsappchat } from "../../utils/helpers"
+import { content } from "../../utils/helpers"
 import background from '../../assets/background.jpg'
 import {CustomImageList, Filter, WhatsAppChat} from "../../components"
-import { KeyboardArrowLeft, KeyboardArrowRight, WhatsApp } from "@mui/icons-material"
+import { KeyboardArrowLeft, KeyboardArrowRight} from "@mui/icons-material"
 import { Link,useNavigate } from "react-router-dom"
 import Plane from '../../assets/plane.webp'
 import Price from '../../assets/prices.png'
@@ -15,7 +15,6 @@ import Calender from '../../assets/calender.webp'
 import Train from '../../assets/train.jpg'
 import America from '../../assets/america.webp'
 import Bridge from '../../assets/bridge.jpg'
-import Whatsapp from '../../assets/whatsapp.png'
 export default function LandingPage(){
   const navigate = useNavigate()
   const [data,setData] = useState([])
@@ -55,8 +54,8 @@ export default function LandingPage(){
         <Container maxWidth sx={{backgroundColor: 'rgba(0, 0, 0, 0.7)',paddingY:"50px"}}>
             <Typography sx={{marginBottom:"40px"}} fontSize="large" fontWeight="bold" color="whitesmoke">Popular Tour Packages / <Link style={{color:"#2196f3", textDecoration:"none"}} to="/packages">View All Tours</Link></Typography>
            
-            <Stack direction="row" spacing={2} sx={{display:"flex",flexDirection:{xs:"column",sm:"row"}, justifyContent:"start",alignItems:"center",cursor:"pointer"}}>
-           
+            <Stack spacing={2} gap={2} direction={{xs:"column", sm:"row"}} justifyContent="center" flexWrap={{sm:"wrap",md:"nowrap"}} alignItems="center" sx={{cursor:"pointer"}}>
+            <KeyboardArrowLeft sx={{backgroundColor:"#2196f3", padding:2, color:"whitesmoke", display:{xs:"none",sm:"none",md:"block"}}}/>
             {/* {combined && combined.length > 3 && <KeyboardArrowLeft sx={{backgroundColor:"#2196f3", padding:2, color:"whitesmoke", display:{xs:"none",sm:"block"}}}/>}
             {combined && combined.length > 0 && combined.map((item)=>{
                     return <Package id={item._id} link={item.link} price={item.price} location={item.location} title={item.title} duration={item.duration} image={item.poster}/>
@@ -68,6 +67,7 @@ export default function LandingPage(){
              <Package price={4500} title="7 DAYS IN ZURICH, ZERMATT" location="Switzerland" duration="7 days" image={Train}/>
              <Package price={3000} image={America} title="America – 2 Days in Lake Tahoe" location="America" duration="7 days"/>
              <Package price={4500} title="7 DAYS IN ZURICH, ZERMATT" location="Switzerland" duration="7 days" image={Bridge}/>
+             <KeyboardArrowRight sx={{backgroundColor:"#2196f3", padding:2, color:"whitesmoke", display:{xs:"none",sm:"none",md:"block"}}}/>
             </Stack>
 
         </Container>
