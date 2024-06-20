e0f7d375cf99d736a0f38eb312ab4c41f147bb25
rebeccawaweru <wawerur95@gmail.com>
2024-06-21 00:34:52 +0300

    suspending website UI

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,6 +2,8 @@ import {createBrowserRouter, RouterProvider} from 'react-router-dom'
 import { useEffect } from 'react';
 import emailjs from "@emailjs/browser";
 import Root from './routes/Root';
+import { Grid,Box, Typography } from '@mui/material';
+import danger from './danger.png'
 const router = createBrowserRouter([
   {path:'*', Component:Root}
 ])
@@ -9,7 +11,12 @@ const router = createBrowserRouter([
 function App() {
   useEffect(() => emailjs.init(process.env.REACT_APP_PUBLIC_KEY), []);
   return (
-  <RouterProvider router={router}/>
+  // <RouterProvider router={router}/>
+   <Grid bgcolor="rgb(203 213 225)" height="100vh" gap={1} container display="flex" flexDirection="column" textAlign="center" justifyContent="center" alignItems="center">
+      <img src={danger} alt="suspension" width={100} height={100}/>
+      <Typography variant="h4" fontWeight="bold">Website Suspended</Typography>
+      <Typography>Contact developer for further information</Typography>
+   </Grid>
 );
 }
 
